aa03080ca447cb10ea97204e8455460764eecabc
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2016-11-06 17:49:16 -0600

    initial stuff

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,49 @@ router.get('/', function(req, res, next) {
   });
 });
 
+var jsonParse = function(body){
+	return JSON.parse(JSON.stringify(body));
+}
+
+router.get('/sample',function(req,res,next){
+  res.send('hai')
+})
+
+router.get('/:hash', function(req,res){
+	const pathToFile = "./../shared/" + req.params.hash + ".json"
+	fs.readFile(pathToFile, 'utf8', (error,content)=> {
+		if (error){
+			return res.status(400).json({error: error});
+		} 
+		return res.json({data: jsonParse(content)});
+	});
+
+});
+
+router.post('/save-link', function(req,res,next) {
+	const bodyParams = req.body['data'];
+	const { type, data, hash } = JSON.parse(bodyParams);
+	saveData = JSON.stringify({
+		options: type,
+		data: data
+	});
+	console.log(saveData);
+	var error = null;
+	if (hash && type && data){
+		const pathToFile = './../shared/'+hash+'.json';
+		fs.writeFile(pathToFile, saveData, (err)=> {
+			if (err) {
+				error = err;
+			}
+
+		});
+
+	} 
+	if (error){
+		return res.status(400).json({error: error});
+	} 
+	return res.json({link: hash});
+
+});
+
 module.exports = router;
